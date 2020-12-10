0ba875715d16d2fa8d2b6c96a55438ad2036c0b1
Alexander <alexandervvugt@gmail.com>
2020-12-10 12:32:53 +0100

    fix(Database) fixed message FK

--- Stexchange/Stexchange/Migrations/20201210112700_message_fk.Designer.cs
+++ Stexchange/Stexchange/Migrations/20201210112700_message_fk.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Stexchange.Data;
 
 namespace Stexchange.Migrations
 {
     [DbContext(typeof(Database))]
-    partial class DatabaseModelSnapshot : ModelSnapshot
+    [Migration("20201210112700_message_fk")]
+    partial class message_fk
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
