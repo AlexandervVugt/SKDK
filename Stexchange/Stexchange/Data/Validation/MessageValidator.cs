b3cb9e09a51ea7562d1d621c392a3fa0522ae630
Lepkem <jordanova@Jordans-MacBook-Air.local>
2020-12-11 15:05:43 +0100

    fix(messagevalidator,chatcontroller) empty messages not allowed

---
+++ Stexchange/Stexchange/Data/Validation/MessageValidator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentValidation;
+using Stexchange.Data.Models;
+
+namespace Stexchange.Data.Validation
+{
+    public class MessageValidator : AbstractValidator<Message>
+    {
+
+        public MessageValidator()
+        {
+            RuleFor(x => x.Content.Trim()).NotEmpty();
+            
+            //todo: handle listingowner.id==responder.id
+            //RuleFor(x => from  in Database.)
+
+            //var verification = (from code in Database.UserVerifications
+            //    where code.Guid == guid
+            //    select code).FirstOrDefault();
+
+        }
+
+    }
+}
