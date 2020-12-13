2c92df7a5da705afe9512de9a33dcbdd83d73b61
wingyeechung <58391243+wingyeechung@users.noreply.github.com>
2020-12-13 16:29:56 +0100

    added minlength to password input, added plantname to searchbar, sorted searchresult on most recent

--- Stexchange/Stexchange/Controllers/TradeController.cs
+++ Stexchange/Stexchange/Controllers/TradeController.cs
@@ -283,33 +283,41 @@ namespace Stexchange.Controllers
         public IActionResult Search(string searchbar, bool search_description) 
         {
             List<Listing> searchList = new List<Listing>();
-
+            
             if (string.IsNullOrEmpty(searchbar) || string.IsNullOrWhiteSpace(searchbar))
             {
                 return RedirectToAction("Trade");
             }
-
+            searchbar = searchbar.ToLower();
             // adds each advertisement to new searchList is the title contains the search value
             foreach (Listing advertisement in _listingCache.Values)
             {
                 if (search_description == true)
                 {
-                    if (advertisement.Title.ToLower().Contains(searchbar.ToLower()) || advertisement.Description.ToLower().Contains(searchbar.ToLower()))
+                    if (advertisement.Title.ToLower().Contains(searchbar) || advertisement.Description.ToLower().Contains(searchbar) || advertisement.NameNl.ToLower().Contains(searchbar))
                     {
                         searchList.Add(advertisement);
+                    }
+                    else if (advertisement.NameLatin != null)
+                    {
+                        if (advertisement.NameLatin.ToLower().Contains(searchbar)) searchList.Add(advertisement);
                     }
                 }
                 else
                 {
-                    if (advertisement.Title.ToLower().Contains(searchbar.ToLower()))
+                    if (advertisement.Title.ToLower().Contains(searchbar) || advertisement.NameNl.ToLower().Contains(searchbar))
                     {
                         searchList.Add(advertisement);
+                    }
+                    else if (advertisement.NameLatin != null)
+                    {
+                        if (advertisement.NameLatin.ToLower().Contains(searchbar)) searchList.Add(advertisement);
                     }
                 }
             }
 
             if(searchList.Count > 0) searchList.ForEach(listing => PrepareListing(ref listing)); 
-            searchList = (from advertisement in searchList orderby advertisement.Title descending select advertisement).ToList();
+            searchList = (from advertisement in searchList orderby advertisement.CreatedAt descending select advertisement).ToList();
             TempData["SearchResults"] = searchList.Count;
 
             return View("trade", new TradeViewModel(searchList));
