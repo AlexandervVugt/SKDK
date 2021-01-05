aee1fb9d7dc1df63ffa3ec3ec97071084e390a6b
Alexander <alexandervvugt@gmail.com>
2021-01-05 11:13:30 +0100

    feat(blocks, ratings): database migration

---
+++ Stexchange/Stexchange/Migrations/20210105101000_block_and_ratings.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.Data.EntityFrameworkCore.Metadata;
+
+namespace Stexchange.Migrations
+{
+    public partial class block_and_ratings : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Blocks",
+                columns: table => new
+                {
+                    blocked_id = table.Column<long>(type: "bigint(20) unsigned", nullable: false),
+                    blocker_id = table.Column<long>(type: "bigint(20) unsigned", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Blocks", x => new { x.blocked_id, x.blocker_id });
+                    table.ForeignKey(
+                        name: "FK_Blocks_Users_blocked_id",
+                        column: x => x.blocked_id,
+                        principalTable: "Users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RatingRequests",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "serial", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    reviewer = table.Column<long>(type: "bigint(20) unsigned", nullable: false),
+                    reviewee = table.Column<long>(type: "bigint(20) unsigned", nullable: false),
+                    created_at = table.Column<DateTime>(type: "datetime", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RatingRequests", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_RatingRequests_Users_reviewee",
+                        column: x => x.reviewee,
+                        principalTable: "Users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_RatingRequests_Users_reviewer",
+                        column: x => x.reviewer,
+                        principalTable: "Users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ratings",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "serial", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    quality = table.Column<byte>(type: "tinyint unsigned", nullable: true),
+                    quantity = table.Column<byte>(type: "tinyint unsigned", nullable: false),
+                    reviewer = table.Column<long>(type: "bigint(20) unsigned", nullable: false),
+                    reviewee = table.Column<long>(type: "bigint(20) unsigned", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ratings", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Ratings_Users_reviewee",
+                        column: x => x.reviewee,
+                        principalTable: "Users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Ratings_Users_reviewer",
+                        column: x => x.reviewer,
+                        principalTable: "Users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Blocks");
+
+            migrationBuilder.DropTable(
+                name: "RatingRequests");
+
+            migrationBuilder.DropTable(
+                name: "Ratings");
+        }
+    }
+}
