fde4ea8b0c874781d66db90738e1af1bf65cb6df
Alexander <alexandervvugt@gmail.com>
2021-01-05 12:54:25 +0100

    feat(MyAccount): method stub & authentication check

--- Stexchange/Stexchange/Controllers/AccountController.cs
+++ Stexchange/Stexchange/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Stexchange.Controllers.Exceptions;
 using Stexchange.Data;
 using System;
 using System.Collections.Generic;
@@ -10,12 +11,24 @@ namespace Stexchange.Controllers
     public class AccountController : StexChangeController
     {
         private readonly Database _db;
+
         public AccountController(Database db)
         {
             _db = db;
         }
+
         public IActionResult MyAccount()
         {
+            try
+            {
+                GetUserId();
+            } catch (InvalidSessionException)
+            {
+                RedirectToAction("Login", "Login");
+            }
+            //TODO: load users data
+            //TODO: load users listings
+            //TODO: load users rating requests
             return View();
         }
     }
