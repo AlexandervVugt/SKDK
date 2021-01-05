1c141033ad55f98b401f94eeebea5454d7d3d797
wingyeechung <58391243+wingyeechung@users.noreply.github.com>
2021-01-05 20:39:40 +0100

    Added: cshtml and css for myaccount, mysettings, myadvertisements, myratingrequests

---
+++ Stexchange/Stexchange/wwwroot/js/MyAccount.js
@@ -0,0 +1,31 @@
+﻿function showRequestForm(form) {
+    let requestform = document.getElementById(form);
+
+    let myadvertisementColumn = document.getElementById("myadvertisementsColumn");
+    let settingsColumn = document.getElementById("mySettingsColumn");
+    let ratingColumn = document.getElementById("ratingreviewColumn");
+    myadvertisementColumn.style.display = "none";
+    settingsColumn.style.display = "none";
+    ratingColumn.style.display = "none";
+    if (form == "requestForm") {
+        myadvertisementColumn.style.display = "block";
+        requestform.style.display = "block";
+    } else {
+        requestform.style.display = "block";
+    }
+}
+
+function closeRequestForm(form) {
+    let myadvertisementColumn = document.getElementById("myadvertisementsColumn");
+    let requestform = document.getElementById(form);
+    requestform.style.display = "none";
+    if (form == 'mySettingsColumn') {
+        myadvertisementColumn.style.display = "block";
+    }
+}
+
+function submitForm(form) {
+    let requestform = document.getElementById(form);
+    closeRequestForm(form);
+    requestform.submit();
+}
