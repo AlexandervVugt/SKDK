162a342067c4ff096af8f21f7a5a14ab143cf51f
Alexander <alexandervvugt@gmail.com>
2021-01-07 14:48:48 +0100

    fix(MyAccount): added migration that makes username and email mutable

---
+++ Stexchange/Stexchange/Migrations/20210107134649_mutable_user_data.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Stexchange.Migrations
+{
+    public partial class mutable_user_data : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_email",
+                table: "Users",
+                column: "email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_username",
+                table: "Users",
+                column: "username",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Users_email",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_username",
+                table: "Users");
+        }
+    }
+}
